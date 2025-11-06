c3dd210cbc371ba0c479c4131b51d8ae44087ccf
hadi dasti <hadidasti98@hotmail.com>
2025-11-06 13:45:31 +0330

    update env for connect to  db

--- todo-app-back/src/app.module.ts
+++ todo-app-back/src/app.module.ts
@@ -12,8 +12,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD,  // مستقیم از env
+      username: process.env.DB_USERNAME || 'todo-user',
+      password: process.env.DB_PASSWORD || "0083499970",
       database: process.env.DB_DATABASE || 'todoapp',
       entities: [],
       synchronize: true,
