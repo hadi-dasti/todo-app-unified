4ec1c9d0477acd8fd4e2ff7a0b462183b12d6ad1
hadi dasti <hadidasti98@hotmail.com>
2025-11-07 16:16:26 +0330

    create first task todo

--- todo-app-back/src/todos/todo.service.ts
+++ todo-app-back/src/todos/todo.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Repository } from 'typeorm';
+import { Todos } from './todo.entity';   
+
+
 
 @Injectable()
 export class TodoService {
+    constructor( @InjectRepository(Todos)
+    private readonly todoRepository:Repository<Todos>){}
+   
+    async findAllTodos(): Promise<Todos[]> {
+        try{
+           const allTodos = await this.todoRepository.find();
+           if(allTodos.length === 0){
+                return [];
+           }
 
-    findAllTodos(){
-        // return this.findAllTodos();
+           const filterTodo = allTodos.filter(todo => todo.done === false);
+
+           
+           return filterTodo;
+                }catch(error){
+            console.log(error)
+        }
     }
 }
