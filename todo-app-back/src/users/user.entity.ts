fb2b5993a9c91508c88158b03c675fc36646069f
hadi Dasti <121736876+hadi-dasti@users.noreply.github.com>
2025-11-15 14:53:23 +0330

    feat: create User entity and Users module

---
+++ todo-app-back/src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import { Entity,Column, PrimaryGeneratedColumn } from "typeorm";
+
+
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+    @Column()
+    username:string;
+
+    @Column()
+    password:string;
+
+    @Column()
+    email : string;
+}
